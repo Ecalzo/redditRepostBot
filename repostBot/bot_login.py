bf4564d8c6a04ba63f1e67d2b7179829195170c2
ecalzolaio <ecalzolaio@gmail.com>
2019-04-28 12:01:37 -0400

    updated config

--- repostBot/bot_login.py
+++ repostBot/bot_login.py
@@ -1,12 +1,13 @@
 import praw
 import os
+from boto.s3.connection import S3Connection
 
 def bot_login():
-    reddit = praw.Reddit(client_id = os.environ["client_id"],
-                client_secret = os.environ["client_secret"],
+    reddit = praw.Reddit(client_id = S3Connection(os.environ["client_id"]),
+                client_secret = S3Connection(os.environ["client_secret"]),
                 user_agent = 'PRAW API tutorial Python Script',
-                username = os.environ["reddit_username"],
-                password = os.environ["reddit_password"])   
+                username = S3Connection(os.environ["reddit_username"]),
+                password = S3Connection(os.environ["reddit_password"]))   
                 
     print("logged in!")
 
