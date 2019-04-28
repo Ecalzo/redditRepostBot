ba7e2bfea2332f80a1ca327f4d07abb3ba5fde69
ecalzolaio <ecalzolaio@gmail.com>
2019-04-28 12:30:52 -0400

    tweaked os.environ.get

--- repostBot/bot_login.py
+++ repostBot/bot_login.py
@@ -1,13 +1,12 @@
 import praw
 import os
-from boto.s3.connection import S3Connection
 
 def bot_login():
-    reddit = praw.Reddit(client_id = os.environ["client_id"],
-                client_secret = os.environ["client_secret"],
+    reddit = praw.Reddit(client_id = os.environ.get("client_id"),
+                client_secret = os.environ.get("client_secret"),
                 user_agent = 'PRAW API tutorial Python Script',
-                username = os.environ["reddit_username"],
-                password = os.environ["reddit_password"])   
+                username = os.environ.get("reddit_username"),
+                password = os.environ.get("reddit_password"))   
                 
     print("logged in!")
 
