381e16728661937ffac03981eb3250954f75e752
ecalzolaio <ecalzolaio@gmail.com>
2019-04-28 12:20:31 -0400

    removed S3

--- repostBot/bot_login.py
+++ repostBot/bot_login.py
@@ -3,11 +3,11 @@ import os
 from boto.s3.connection import S3Connection
 
 def bot_login():
-    reddit = praw.Reddit(client_id = S3Connection(os.environ["client_id"]),
-                client_secret = S3Connection(os.environ["client_secret"]),
+    reddit = praw.Reddit(client_id = os.environ["client_id"],
+                client_secret = os.environ["client_secret"],
                 user_agent = 'PRAW API tutorial Python Script',
-                username = S3Connection(os.environ["reddit_username"]),
-                password = S3Connection(os.environ["reddit_password"]))   
+                username = os.environ["reddit_username"],
+                password = os.environ["reddit_password"])   
                 
     print("logged in!")
 
