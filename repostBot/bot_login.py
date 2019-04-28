e801fcadc1f8bc2a1d39e0b8518b942c46486913
ecalzolaio <ecalzolaio@gmail.com>
2019-04-28 12:37:18 -0400

    tweaked os.getenv

--- repostBot/bot_login.py
+++ repostBot/bot_login.py
@@ -2,11 +2,11 @@ import praw
 import os
 
 def bot_login():
-    reddit = praw.Reddit(client_id = os.environ.get("client_id"),
-                client_secret = os.environ.get("client_secret"),
+    reddit = praw.Reddit(client_id = os.getenv("client_id"),
+                client_secret = os.getenv("client_secret"),
                 user_agent = 'PRAW API tutorial Python Script',
-                username = os.environ.get("reddit_username"),
-                password = os.environ.get("reddit_password"))   
+                username = os.getenv("reddit_username"),
+                password = os.getenv("reddit_password"))   
                 
     print("logged in!")
 
