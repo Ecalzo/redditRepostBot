15ee205f136390f00835138260202acf8fd65ea1
ecalzolaio <ecalzolaio@gmail.com>
2019-12-06 14:14:23 -0500

    remove aws scripts. Add exception logging

--- repostBot/bot.py
+++ repostBot/bot.py
@@ -4,8 +4,15 @@ import pandas as pd
 import bot_login
 from datetime import datetime
 import pymongo
+import logging
 import mongo_setup
 import random
+
+logging.basicConfig(filename='main.log',
+                    level=logging.INFO,
+                    filemode='a',
+                    format='%(name)s - %(levelname)s - %(message)s')
+
 
 def get_post(sub='pics'):
     '''
@@ -17,25 +24,25 @@ def get_post(sub='pics'):
     current_time = datetime.now().strftime("%m-%d-%Y")
     # dictionary to store the fetched info
     redditInfo = {}
-    randomInt = random.randint(3,40)
+    randomInt = random.randint(3, 40)
     # obtaining the top post info and passing it to a dict
-    # Use the top of the week and go a few posts down for an inconspicuous repost
+    # Use the top of the week and go a few posts down
     for submission in reddit.subreddit(sub).top('week', limit=randomInt):
         redditInfo['title'] = submission.title
         redditInfo['url'] = submission.url
         redditInfo['subreddit'] = submission.subreddit
         redditInfo['time'] = current_time
     # Append the overall user karma at this point in time
-    # subroutine to clean the user karma (remove objects incompatible with Mongo)
+    # subroutine to clean the user karma
+    # (remove objects incompatible with Mongo)
     karma = reddit.user.karma()
     cleaned_karma = {}
     for key, value in karma.items():
         # extract subreddit display name as str
         new_key = key.display_name
         # write to cleaned karma dictionary
-        cleaned_karma[new_key] = value    
+        cleaned_karma[new_key] = value
     redditInfo['karma'] = cleaned_karma
-    
     # return the dictionary (to be appended to a list)
     return redditInfo
 
@@ -43,27 +50,29 @@ def get_post(sub='pics'):
 def submit_post(post_dict):
     '''
     Posts scraped post to the subreddit in the dictionary
-    Does not handle non-link (text) posts yet 
+    Does not handle non-link (text) posts yet
     '''
-    # this will repost the fetched post and return the post id for later tracking
-    submitted = post_dict['subreddit'].submit(title=post_dict['title'], url=post_dict['url'])
+    # this will repost the fetched post and return the post id for tracking
+    submitted = post_dict['subreddit'].submit(title=post_dict['title'],
+                                              url=post_dict['url'])
     # Append to a list tracking these items
     return submitted
-    
+
 
 def scrape_post_collect():
-    # for test purposes, we will try this ten times
-    subreddits = ['pics','me_irl','dankmemes','eyebleach']
-    # TODO: change to while true: to run indefinitely
-    for _ in range(0,10):
+    # example subreddits - could make these argparse flags
+    subreddits = ['pics', 'me_irl', 'dankmemes', 'eyebleach']
+    while True:  # better to just schedule this with cron
         for sub in subreddits:
             try:
                 print("Getting post")
                 # get a top post from r/pics
                 post_info = get_post(sub)
                 # log for debugging
-                print(post_info['title'],post_info['url'])
-                print('sleeping... ') 
+                logging.info('{} {}'.format(post_info['title'],
+                                            post_info['url']))
+                print('sleeping... ')
+                logging.info('sleeping...')
                 # sleep and repost in 24 hours
                 time.sleep(12000)
                 # resubmit the post
@@ -83,16 +92,16 @@ def scrape_post_collect():
                     # insert post into MongoDB Collection
                     print('attempting to initalize and push to mongoDB')
                     mongo_setup.mongo_login_and_insert(post)
-                except Exception as e: 
+                except Exception as e:
                     print('could not initialize MongoDB')
-                    print(e)
+                    logging.exception('An Exception Occured')
                 print("posted something new at: ", datetime.now())
             except Exception as e:
                 print("failed at: ", datetime.now())
                 print(e)
-    
+                logging.exception('An Exception Occured')
+
+
 # Initialize the main function
 if __name__ == "__main__":
     scrape_post_collect()
-        
-    
